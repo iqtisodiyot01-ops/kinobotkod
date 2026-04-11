9b63f43b9971076244b18d4c754da6f3466065e2
iqtisodiyot01-ops <iqtisodiyot01@gmail.com>
2026-04-11 20:06:05 +0500

    add: config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").replace(" ", "").strip()
+
+admin_ids_str = os.getenv("ADMIN_IDS", "").strip()
+ADMIN_IDS = list(map(int, admin_ids_str.split(","))) if admin_ids_str else []
+
+CHANNELS = [c.strip() for c in os.getenv("CHANNELS", "").split(",") if c.strip()]
+MOVIE_CHANNEL = os.getenv("MOVIE_CHANNEL", "")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
